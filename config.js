fa3bbc960497b875a0b67506b5cb0c2276758d69
sudhanshusingh-g <sudsingh32@gmail.com>
2024-09-20 13:07:21 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primaryblue: "hsl(231, 69%, 60%)",
+        primaryred: "hsl(0, 94%, 66%)",
+        neutralgrayblue: "hsl(229, 8%, 60%)",
+        neutraldarkblue: "hsl(229, 31%, 21%)",
+      },
+      fontFamily:{
+        rubik:['Rubik','sans-serif']
+      },
+    },
+  },
+};
